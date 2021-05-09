fd0d1d40ba0638187ecdf67053031b349b9e0d56
amcrae <ajmcrae@gmail.com>
2021-05-09 11:10:47 +0000

    Render emotion confidences as table.

--- sentimentAnalyzeClient/src/EmotionTable.js
+++ sentimentAnalyzeClient/src/EmotionTable.js
@@ -3,20 +3,30 @@ import './bootstrap.min.css';
 
 class EmotionTable extends React.Component {
     render() {
+      // There is no map() function on Javascript Map objects, only on lists.
+      // It makes more sense to render by iterating through the Map keys,
+      // however we were asked to use the map() function to render.
+      // So I have decided to transform the data into a nested list format
+      // which can be rendered easier by a map() producing JSX elements.
+      let toRender = [];
+      for (let k in this.props.emotions) {
+          toRender.push( [ k, this.props.emotions[k] ] );
+      }
       return (  
         <div>
-          {/*You can remove this line and the line below. */}
-          {JSON.stringify(this.props.emotions)}
-          <table className="table table-bordered">
+          <table className="table table-bordered emotions">
+            <thead><th>Emotion</th><th>Confidence Level</th></thead>
             <tbody>
-            {
-                //Write code to use the .map method that you worked on in the Hands-on React lab to extract the emotions
-            }
+                { 
+                    toRender.map(
+                        (li) => <tr> <td>{li[0]}</td><td>{li[1]}</td> </tr>
+                    )
+                }
             </tbody>
           </table>
           </div>
           );
-        }
+    }
     
 }
 export default EmotionTable;
